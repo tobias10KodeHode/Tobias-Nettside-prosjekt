934a38274b903f8253104db199b2b291c2ec3352
tobias10kodehode <tobias10kodehode@gmail.com>
2024-01-28 11:09:11 +0100

    Added function: Button to go to top of the page.

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function isEven(number) {
-    return number % 2 === 0;
-}
+// Tilbake til toppen funksjon
 
-let s = "";
-let a = [5, 8, 9, 0, 66, 18];
-
-for (let i = 1; i < a.length; i++) {
-    if (isEven(a[i]) && a[i - 1] % 3 === 0) {
-        s += a[i] / 2;
-    } else {
-        s += a[i] * 2;
-    }
-}
-
-let url = "https://nixus.no/" + s;
-console.log(url);
+const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
